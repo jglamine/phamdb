ea46a7dcc37b9ba446bc259b105ed29587aefb7f
James Lamine <jglamine2@gmail.com>
2015-04-21 12:43:08 +0000

    Migrate existing database in docker script.

--- docker/start-mysql.sh
+++ docker/start-mysql.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # If the database directory is empty, copy the initial database to it.
-if [ ! -d /dockerdata/mysql ]; then
-	cp -rp /var/lib/mysql /dockerdata/mysql
+# Otherwise, migrate the database.
+if [ ! /dockerdata/mysql/ibdata1 ];
+then
+	cp -r /var/lib/mysql /dockerdata/mysql
+	/etc/init.d/mysql start
+else
+	/etc/init.d/mysql start
+	python /home/docker/code/webphamerator/manage.py db upgrade --directory /home/docker/code/webphamerator/migrations
 fi
-
-/etc/init.d/mysql start
