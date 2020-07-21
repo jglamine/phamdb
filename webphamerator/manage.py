5b20f9943c849737fa437121a18cb8b247d7e0eb
chg60 <chg60@pitt.edu>
2020-07-21 17:42:25 -0400

    possible fixes did not work

--- webphamerator/manage.py
+++ webphamerator/manage.py
@@ -1,10 +1,10 @@
-from webphamerator import app
+from webphamerator.app import app, db
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from webphamerator.app.models import *
 
-migrate = Migrate(app.app, app.db)
+migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
