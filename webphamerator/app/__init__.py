16bab9b200ff847b1a8b4d283b56a29a121b6577
Lawrence Abad <laa89@pitt.edu>
2020-08-12 09:04:34 -0400

    Functionalized flask app creation

--- webphamerator/app/__init__.py
+++ webphamerator/app/__init__.py
@@ -1,11 +1,33 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# from webphamerator.app import filters, auth, views, api, models, tasks
+from webphamerator.flask import filters, auth, views, api, models, tasks
 from celery import Celery
 
 
-def make_celery(a):
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object('webphamerator.config')
+
+    if not os.path.exists(app.config['GENBANK_FILE_DIR']):
+        os.makedirs(app.config['GENBANK_FILE_DIR'])
+    if not os.path.exists(app.config['DATABASE_DUMP_DIR']):
+        os.makedirs(app.config['DATABASE_DUMP_DIR'])
+
+    db = SQLAlchemy(app)
+    celery = make_celery(app)
+
+    if not app.debug:
+        import logging
+        from logging.handlers import RotatingFileHandler
+        file_handler = RotatingFileHandler('webphamerator.log', 'a', 1 * 1024 * 1024, 10)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        app.logger.setLevel(logging.INFO)
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+def build_celery(a):
     celery = Celery(a.import_name, broker=a.config['CELERY_BROKER_URL'])
     celery.conf.update(a.config)
     TaskBase = celery.Task
@@ -22,23 +44,4 @@ def make_celery(a):
     return celery
 
 
-app = Flask(__name__)
 
-app.config.from_object('webphamerator.config')
-
-if not os.path.exists(app.config['GENBANK_FILE_DIR']):
-    os.makedirs(app.config['GENBANK_FILE_DIR'])
-if not os.path.exists(app.config['DATABASE_DUMP_DIR']):
-    os.makedirs(app.config['DATABASE_DUMP_DIR'])
-
-db = SQLAlchemy(app)
-celery = make_celery(app)
-
-if not app.debug:
-    import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('webphamerator.log', 'a', 1 * 1024 * 1024, 10)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
